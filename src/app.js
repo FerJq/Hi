761227f827f5af85e8444617be0360b4695cf818
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-03-13 18:23:14 -0400

    transition of background colors with js and css

---
+++ src/app.js
@@ -0,0 +1,11 @@
+let bg = document.querySelector("body");
+
+function background_theme() {
+  if (bg.classList.contains("color-theme")) {
+    bg.classList.remove("color-theme");
+  } else {
+    bg.classList.add("color-theme");
+  }
+}
+
+setInterval(background_theme, 1000);
